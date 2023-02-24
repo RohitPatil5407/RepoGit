8ee176d1a35f1769dabee98286d3b386243c160f
RohitPatil5407 <rohitpatil5407@gmail.com>
2023-02-24 13:05:34 +0530

    commited

--- src/test/java/tokenPackage/GitTest.java
+++ src/test/java/tokenPackage/GitTest.java
@@ -3,7 +3,7 @@ package tokenPackage;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("It is first git project");
 
 	}
 
